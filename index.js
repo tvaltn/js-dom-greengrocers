01f8f4c5aae77b7cc59a40f9e161a90cf33d18b3
tvaltn <valtniemi@gmail.com>
2024-09-24 11:03:42 +0200

    extensions done

--- index.js
+++ index.js
@@ -3,65 +3,86 @@ const state = {
     {
       id: "001-beetroot",
       name: "beetroot",
-      price: 0.35
+      price: 0.35,
+      type: "vegetable"
     },
     {
       id: "002-carrot",
       name: "carrot",
-      price: 0.35
+      price: 0.35,
+      type: "vegetable"
     },
     {
       id: "003-apple",
       name: "apple",
-      price: 0.35
+      price: 0.35,
+      type: "fruit"
     },
     {
       id: "004-apricot",
       name: "apricot",
-      price: 0.35
+      price: 0.35,
+      type: "fruit"
     },
     {
       id: "005-avocado",
       name: "avocado",
-      price: 0.35
+      price: 0.35,
+      type: "fruit"
     },
     {
       id: "006-bananas",
       name: "bananas",
-      price: 0.35
+      price: 0.35,
+      type: "fruit"
     },
     {
       id: "007-bell-pepper",
       name: "bell pepper",
-      price: 0.35
+      price: 0.35,
+      type: "vegetable"
     },
     {
       id: "008-berry",
       name: "berry",
-      price: 0.35
+      price: 0.35,
+      type: "fruit"
     },
     {
       id: "009-blueberry",
       name: "blueberry",
-      price: 0.35
+      price: 0.15,
+      type: "fruit"
     },
     {
       id: "010-eggplant",
       name: "eggplant",
-      price: 0.35
+      price: 0.25,
+      type: "vegetable"
     }
   ],
-  cart: []
+  cart: [],
+  filter: "all",
+  sort: "id"
 };
 
 const storeList = document.querySelector(".store--item-list")
 const cartList = document.querySelector(".cart--item-list")
 const totalNumber = document.querySelector(".total-number")
 
+// -------- STORE -----------
 function renderStoreItems() {
+  while (storeList.firstChild) storeList.removeChild(storeList.firstChild)
+
+  if (state.sort === "id") state.items.sort((a, b) => a.id.localeCompare(b.id))
+  if (state.sort === "price") state.items.sort((a, b) => a.price - b.price)
+  if (state.sort === "name") state.items.sort((a, b) => a.name.localeCompare(b.name))
+
   state.items.map((item) => {
-    let li = renderStoreItem(item)
-    storeList.appendChild(li)
+    if (item.type === state.filter || state.filter === "all") {
+      let li = renderStoreItem(item)
+      storeList.appendChild(li)
+    }
   })
 }
 
@@ -71,7 +92,11 @@ function renderStoreItem(storeItem) {
   const div = document.createElement("div")
   div.setAttribute("class", "store--item-icon")
   const img = document.createElement("img")
-  img.setAttribute("src", "assets/icons/" + storeItem.id + ".svg")
+  if ("imgurl" in storeItem) {
+    img.setAttribute("src", storeItem.imgurl)
+  } else {
+    img.setAttribute("src", "assets/icons/" + storeItem.id + ".svg")
+  }
   img.setAttribute("alt", storeItem.name)
 
   const button = document.createElement("button")
@@ -87,6 +112,93 @@ function renderStoreItem(storeItem) {
   return li
 }
 
+function filterButtons() {
+  const filterBtns = document.querySelector(".filter-btns")
+
+  const allButton = document.createElement("button")
+  allButton.innerText = "All"
+  allButton.addEventListener("click", () => {
+    state.filter = "all"
+    renderStoreItems()
+  })
+
+  const fruitButton = document.createElement("button")
+  fruitButton.innerText = "Fruits"
+  fruitButton.addEventListener("click", () => {
+    state.filter = "fruit"
+    renderStoreItems()
+  })
+
+  const vegetableButton = document.createElement("button")
+  vegetableButton.innerText = "Vegetables"
+  vegetableButton.addEventListener("click", () => {
+    state.filter = "vegetable"
+    renderStoreItems()
+  })
+
+  filterBtns.appendChild(allButton)
+  filterBtns.appendChild(fruitButton)
+  filterBtns.appendChild(vegetableButton)
+}
+
+function sortButtons() {
+  const sortBtns = document.querySelector(".sort-btns")
+
+  const regularSort = document.createElement("button")
+  regularSort.innerText = "Regular Sort"
+  regularSort.addEventListener("click", () => {
+    state.sort = "id"
+    renderStoreItems()
+  })
+
+  const priceSort = document.createElement("button")
+  priceSort.innerText = "Price Sort"
+  priceSort.addEventListener("click", () => {
+    state.sort = "price"
+    renderStoreItems()
+  })
+
+  const nameSort = document.createElement("button")
+  nameSort.innerText = "Name Sort"
+  nameSort.addEventListener("click", () => {
+    state.sort = "name"
+    renderStoreItems()
+  })
+
+  sortBtns.appendChild(regularSort)
+  sortBtns.appendChild(priceSort)
+  sortBtns.appendChild(nameSort)
+}
+
+function submitNewItemButton() {
+  const submitButton = document.getElementById("create-item-button")
+  submitButton.addEventListener("click", () => {
+    createNewItem()
+  })
+}
+
+function createNewItem() {
+  const namee = document.getElementById("create-item-name").value
+  const price = parseFloat(document.getElementById("create-item-price").value)
+  const type = document.getElementById("create-item-type").value
+  const imgurl = document.getElementById("create-item-imgurl").value
+
+  if (namee && price && type && imgurl) {
+    const item = 
+    {
+      id: "0" + state.items.length + "-" + namee,
+      name: namee,
+      price: price,
+      type: type,
+      imgurl: imgurl
+    }
+    state.items.push(item)
+    renderStoreItems()
+  }
+}
+
+
+// -------- CART -----------
 function renderCartItems() {
   while (cartList.firstChild) cartList.removeChild(cartList.firstChild)
 
@@ -124,7 +236,11 @@ function renderCartItem(storeItem) {
   const li = document.createElement("li")
   const img = document.createElement("img")
   img.setAttribute("class", "cart--item-icon")
-  img.setAttribute("src", "assets/icons/" + storeItem.id + ".svg")
+  if ("imgurl" in storeItem) {
+    img.setAttribute("src", storeItem.imgurl)
+  } else {
+    img.setAttribute("src", "assets/icons/" + storeItem.id + ".svg")
+  }
   img.setAttribute("alt", storeItem.name)
 
   const p = document.createElement("p")
@@ -171,3 +287,6 @@ function increaseCartItem(storeItem) {
 }
 
 renderStoreItems()
+filterButtons()
+sortButtons()
+submitNewItemButton()
